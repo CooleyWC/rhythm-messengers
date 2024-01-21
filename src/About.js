e61ffce3667625d2a516bc085aa52fde42ae5a0c
Ruth <ruro122020@gmail.com>
2024-01-20 19:06:22 -0500

    add text and button to about component

--- src/About.js
+++ src/About.js
@@ -4,6 +4,13 @@ function About(props) {
     return (
         <div>
             <h1>About</h1>
+            <div>
+              <p>The Rhythm Messengers are an adaptable crew of highly animated drummers and entertainers to elevate the energy of your next event! </p>
+              <p>We are a collection of performers drawing from Chicago’s best drumming productions.  We’ll tailer make our show to best fit your event needs and venue.</p>
+            </div>
+            <div>
+              <button>Book Now</button>
+            </div>
         </div>
     );
 }
