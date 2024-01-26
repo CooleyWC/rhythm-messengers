728f7b50bc335147affcbcb1ef770ff100d6605a
Will Cooley <william.cooley@gmail.coom>
2024-01-26 14:29:43 -0500

    adjusted background image and added video

--- src/Title.js
+++ src/Title.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import {Grid} from '@mui/material';
+import {styled} from '@mui/system';
+
+const TitleContainer = styled(Grid)({
+    width: '100%',
+    height: '60vh',
+    '@media (max-width:600px':{
+        height: '40vh',
+    }
+})
+
+const BackgroundImage = styled('div')({
+    backgroundRepeat: 'no-repeat',
+    height: '100%',
+    width: '100%',
+    backgroundSize: 'cover',
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black'
+})
+
+const Image = styled('img')({
+    width: '50%',
+    height: 'auto',
+    objectFit: 'cover',
+})
 
 
 function Title(props) {
+
     return (
-        <Grid className = 'title-container'>
+        <TitleContainer container>
             <Grid item xs={12}>
-                <div className="bg-image">
-                    <img src='media/RMELogo300-2-ver-4.png' alt='drum-logo' style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-                </div>
+                <BackgroundImage>
+                    <Image src='media/RMELogo300-2-ver-7.jpeg' alt='drum-logo' />
+                </BackgroundImage>
             </Grid>
-        </Grid>
+        </TitleContainer>
     );
 }
 
--- src/Video.js
+++ src/Video.js
@@ -1,20 +1,41 @@
 import React from 'react';
 import { CardMedia, Container } from '@mui/material';
+import {styled} from '@mui/system';
+
+const VideoContainer = styled(Container)(({theme}) => ({
+   
+    backgroundColor: 'black',
+    paddingTop: theme.spacing(60),
+    [theme.breakpoints.down('sm')]: {
+        paddingTop: theme.spacing(2),
+    },
+    width: '100%'
+
+}));
+
+const StyledCardMedia = styled(CardMedia)({
+    width: '100%',
+    // height: '100%',
+    margin: 0,
+    padding: 0,
+    background: 'black',
+    objectFit: 'cover'
+})
 
 function Video(props) {
     return (
-        <Container>
-            <CardMedia 
+        <VideoContainer>
+            <StyledCardMedia 
                 component='video'
                 src='/media/RMEWebsiteTeaser.mp4'
                 autoPlay
                 loop
                 muted
-                width="100%"
+                // width="100%"
                 title="RM-promo-video"
             />
                
-        </Container>
+        </VideoContainer>
     );
 }
 
