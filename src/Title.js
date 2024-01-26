6f2603757aa482c488718fc31ef7aa9ec74b60d4
Will Cooley <william.cooley@gmail.coom>
2024-01-25 19:44:34 -0500

    added video in Video component. added Material UI to help with background image sizing

--- src/Title.js
+++ src/Title.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import {Grid} from '@mui/material';
 
 
 function Title(props) {
     return (
-        <div className = 'title-container'>
-            <div class="bg-image">
-
-            </div>
-        </div>
+        <Grid className = 'title-container'>
+            <Grid item xs={12}>
+                <div className="bg-image">
+                    <img src='media/RMELogo300-2-ver-4.png' alt='drum-logo' style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+                </div>
+            </Grid>
+        </Grid>
     );
 }
 
--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { CardMedia, Container } from '@mui/material';
 
 function Video(props) {
     return (
-        <div>
-            <h1>Background Video</h1>
-        </div>
+        <Container>
+            <CardMedia 
+                component='video'
+                src='/media/RMEWebsiteTeaser.mp4'
+                autoPlay
+                loop
+                muted
+                width="100%"
+                title="RM-promo-video"
+            />
+               
+        </Container>
     );
 }
 
