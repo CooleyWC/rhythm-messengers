6120c4335966d3f4f2a5b9e326271f9ddacc48a6
Ruth <ruro122020@gmail.com>
2024-01-27 13:06:27 -0500

    about section in progress

--- src/about/About.js
+++ src/about/About.js
@@ -3,34 +3,38 @@ import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
 import { Box, Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
 import Card from './Card'
 
-function About(props) {
-  return (
-    <Container id='about'>
-      <Typography variant="h1" align="center" gutterBottom>
-        About
-      </Typography>
-      <Grid container justify="center">
 
-        <Grid xs={12} md={12}>
-          <Typography variant="body1" paragraph align='center'>
+
+
+function About(props) {
+
+  return (
+    <div style={{ backgroundColor: 'black' }}>
+      <Container id='about' sx={{ bgcolor: 'black', color: 'white' }}>
+        <Typography variant="h2" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
+          About
+        </Typography>
+        <Grid   >
+          <Typography variant="body1" paragraph align='center' >
             The Rhythm Messengers are an adaptable crew of highly animated drummers and entertainers to elevate the energy of your next event!
           </Typography>
-          <Typography variant="body1" paragraph align='center'>
+          <Typography variant="body1" paragraph align='center' >
             We are a collection of performers drawing from Chicago’s best drumming productions. We’ll tailor-make our show to best fit your event needs and venue.
           </Typography>
         </Grid>
-        <Grid xs={12} md={12}>
-          <Typography variant="h2" align="center" gutterBottom>
-            ENERGY
+        <Grid >
+          <Typography variant="h3" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
+            Energy
           </Typography>
-          <Typography variant="body1" paragraph align='center'>
+          <Typography variant="body1" paragraph align='center' >
             Ignite your event with our choreographed, high-energy drumming that will get your guests jumping out of their chairs!
           </Typography>
         </Grid>
-        <Grid xs={12} md={12}>
-          <Typography variant="h2" align="center" gutterBottom>
+        <Grid >
+          <Typography variant="h3" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
             'BOOM' Your Audience
           </Typography>
           <Typography variant="body1" paragraph align='center'>
@@ -39,15 +43,15 @@ function About(props) {
           <Typography variant="body1" paragraph align='center'>
             Call today and give your guests - a BOOM!
           </Typography>
-          <Box textAlign='center'>
-            <Button variant="contained" sx={{ backgroundColor: '#FFC080', ':hover': { bgcolor: '#f77a00' } }} href='#' >
+          <Box textAlign='center' >
+            <Button variant="contained" sx={{ backgroundColor: '#f77a00', ':hover': { backgroundColor: '#f77a00' } }} href='#' >
               Book Now
             </Button>
           </Box>
         </Grid>
-      </Grid>
 
-      {/* <div>
+
+        {/* <div>
                 <Card 
                 title='TRADE SHOWS' 
                 description="Stop traffic at your booth with an attraction they won't forget."/>
@@ -58,7 +62,8 @@ function About(props) {
                 title='SESSIONS' 
                 description="Kick off the meeting with an electrifying and high energy atmosphere."/>
               </div> */}
-    </Container>
+      </Container>
+    </div>
   );
 }
 
