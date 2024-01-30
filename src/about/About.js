b53afcd20ab87f46205935c7bed0de2df4efaaf8
Will Cooley <william.cooley@gmail.coom>
2024-01-30 14:17:21 -0500

    updated About component styling and added photo

--- src/about/About.js
+++ src/about/About.js
@@ -1,70 +1,102 @@
 import React from 'react';
-import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid'
-import Button from '@mui/material/Button'
-import { Box, Typography } from '@mui/material';
-import Divider from '@mui/material/Divider';
-import Card from './Card'
-
-
+import { Box, CardMedia, Typography } from '@mui/material';
+import BatteryChargingFullIcon from '@mui/icons-material/BatteryChargingFull';
 
 
 const About=(props) =>{
 
+  const boxStyle = {
+    backgroundColor: 'white'
+  }
+
+  const aboutTitleStyle = {
+    marginTop:'40px',
+    marginBottom: '40px',
+    paddingTop: '20px',
+    paddingBottom: '40px',
+    backgroundColor: 'black',
+   
+  }
+
+  const energyBoxStyle = {
+    // paddingTop: '100px'
+  }
+
   return (
     <div style={{ backgroundColor: 'black' }}>
-      <Container sx={{ bgcolor: 'black', color: 'white' }}>
-        <Typography variant="h2" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
-          About
-        </Typography>
-        <Grid   >
-          <Typography variant="body1" paragraph align='center' >
-            The Rhythm Messengers are an adaptable crew of highly animated drummers and entertainers to elevate the energy of your next event!
-          </Typography>
-          <Typography variant="body1" paragraph align='center' >
-            We are a collection of performers drawing from Chicago’s best drumming productions. We’ll tailor-make our show to best fit your event needs and venue.
-          </Typography>
+      <Box style={boxStyle}>
+        <Grid container>
+          <Grid item xs={12} style={aboutTitleStyle}>
+            <Typography variant="h2" gutterBottom sx={{ fontSize: '48px', color: '#f77a00', paddingBottom: '30px', textAlign: "center" }}>
+              About
+            </Typography>
+            <Typography variant='body1'  style={{color: 'white', fontSize: '22px', fontWeight: '300', marginLeft: '100px', marginRight: '100px'}}>
+              The Rhythm Messengers are an adaptable crew of highly animated drummers and entertainers to elevate the energy of your next event!<br/><br/>
+              We are a collection of performers drawing from Chicago’s best drumming productions. We’ll tailor-make our show to best fit your event needs and venue.
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid >
-          <Typography variant="h3" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
-            Energy
-          </Typography>
-          <Typography variant="body1" paragraph align='center' >
-            Ignite your event with our choreographed, high-energy drumming that will get your guests jumping out of their chairs!
-          </Typography>
+      </Box>
+      <Box style={energyBoxStyle}>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Typography variant="h2" align="center" gutterBottom sx={{ fontSize: '48px', color: '#007FFF', paddingTop: '100px', paddingBottom: '30px' }}>
+              Energy
+            </Typography>
+            <BatteryChargingFullIcon style={{color: '#007FFF', fontSize:'60px', margin: '0 auto', display: 'block', paddingBottom: '30px'}}/>
+            <Typography variant='body1' paragraph style={{color: 'white', fontSize: '18px', fontWeight: '300', paddingLeft: '80px', paddingBottom: '30px'}}>
+              The Rhythm Messengers are an adaptable crew of highly animated drummers and entertainers to elevate the energy of your next event!
+            </Typography>
+          </Grid>
+          <Grid item xs={4} style={{display: 'flex', justifyContent:'flex-end'}}>
+            <CardMedia
+              component='img'
+              src='/media/RME-energy-photo.png'
+              alt='drum-energy'
+              style={{maxWidth: '100%', height: 'auto', objectFit: 'cover'}}
+            >
+            </CardMedia>
+          </Grid>
         </Grid>
-        <Grid >
-          <Typography variant="h3" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
-            'BOOM' Your Audience
-          </Typography>
-          <Typography variant="body1" paragraph align='center'>
-            Your event will take center stage when the Rhythm Messengers make their appearance. Your guests will jump, stomp, and dance your event away.
-          </Typography>
-          <Typography variant="body1" paragraph align='center'>
-            Call today and give your guests - a BOOM!
-          </Typography>
-          {/* <Box textAlign='center' >
-            <Button variant="contained" sx={{ backgroundColor: '#f77a00', ':hover': { backgroundColor: '#f77a00' } }} href='#' >
-              Book Now
-            </Button>
-          </Box> */}
-        </Grid>
+      </Box>
 
-
-        {/* <div>
-                <Card 
-                title='TRADE SHOWS' 
-                description="Stop traffic at your booth with an attraction they won't forget."/>
-                <Card 
-                title='FESTIVALS' 
-                description="Main stage or impromptu, We light the fuse to festival fun."/>
-                <Card 
-                title='SESSIONS' 
-                description="Kick off the meeting with an electrifying and high energy atmosphere."/>
-              </div> */}
-      </Container>
     </div>
   );
 }
 
 export default About;
+
+
+{/* <Container sx={{ bgcolor: 'black', color: 'white' }}>
+<Typography variant="h2" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
+  About
+</Typography>
+<Grid   >
+  <Typography variant="body1" paragraph align='center' >
+    The Rhythm Messengers are an adaptable crew of highly animated drummers and entertainers to elevate the energy of your next event!
+  </Typography>
+  <Typography variant="body1" paragraph align='center' >
+    We are a collection of performers drawing from Chicago’s best drumming productions. We’ll tailor-make our show to best fit your event needs and venue.
+  </Typography>
+</Grid>
+<Grid >
+  <Typography variant="h3" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
+    Energy
+  </Typography>
+  <Typography variant="body1" paragraph align='center' >
+    Ignite your event with our choreographed, high-energy drumming that will get your guests jumping out of their chairs!
+  </Typography>
+</Grid>
+<Grid >
+  <Typography variant="h3" align="center" gutterBottom sx={{ fontSize: '48px', color: '#f77a00' }}>
+    'BOOM' Your Audience
+  </Typography>
+  <Typography variant="body1" paragraph align='center'>
+    Your event will take center stage when the Rhythm Messengers make their appearance. Your guests will jump, stomp, and dance your event away.
+  </Typography>
+  <Typography variant="body1" paragraph align='center'>
+    Call today and give your guests - a BOOM!
+  </Typography>
+ 
+</Grid> */}
