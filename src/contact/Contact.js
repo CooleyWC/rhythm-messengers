26dc9e29c703d4fde3cd0af3da853efe3f97ea95
Ruth <ruro122020@gmail.com>
2024-02-02 12:02:58 -0500

    form complete

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -15,7 +15,8 @@ import Check from '@mui/icons-material/Check';
 import Container from '@mui/material/Container';
 import Input from '@mui/joy/Input';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Card from './Card'
 
 const Contact = (props) => {
   const [email, setEmail] = React.useState('')
@@ -131,6 +132,22 @@ const Contact = (props) => {
           </FormControl>
         </Grid>
       </form>
+
+      <Card
+        name="THOMAS BENKO"
+        title="Band Leader"
+        email=""
+        phoneNumber=" 1-630-415-9521" />
+      <Card
+        name="LIVE ART INTERNATIONAL"
+        title="Agent - Dayna Malow"
+        email="info@liveartinternational.com"
+        phoneNumber="1-800-985-9330" />
+      <Card
+        name="SF Agent"
+        title="Pinnacle - Nathan Kelly"
+        email=""
+        phoneNumber="1-800-769-3322" />
     </Container>
   );
 }
