2309bfbe6bf2fb5e2cc5bda60e0009a44ba5273d
Will Cooley <william.cooley@gmail.coom>
2024-02-03 23:06:26 -0500

    changed title font and styling

--- src/Title.js
+++ src/Title.js
@@ -1,52 +1,50 @@
 import React from 'react';
-import {Grid, CardMedia, Typography} from '@mui/material';
+import {Grid, CardMedia} from '@mui/material';
+import Stack from '@mui/material/Stack';
+
 
 
 function Title(props) {
 
-    const titleText = 'Rhythm Messengers'
-
     const gridContainerStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
         backgroundColor: 'black',
-        overflow: 'hidden',
         margin: 0,
         padding: 0,
-
     }
 
     const imageStyle = {
         maxWidth: '75%',
-        maxHeight: '75%',
-        marginTop: '20px',
-        objectFit: 'contain'
+        maxHeight: '80%',
+        marginTop: '25px',
+        objectFit: 'cover'
     }
 
-    const typeStyle = {
-        color: 'white',
-        textAlign: 'center',
-        '@media (max-width:600px)': {
-            textAlign: 'center',
-        }
-
+    const titleStyle = {
+        maxWidth: '75%',
+        marginTop: '-55px',
     }
 
     return (
-        <Grid container style={gridContainerStyle}>
-            <CardMedia
-                component='img'
-                src='media/RMELogo300-2-no-background.png' 
-                alt='drum-logo' 
-                style={imageStyle}
-                >
-            </CardMedia>
-            <Typography variant='h3' style={typeStyle}>
-                {titleText}
-            </Typography> 
+        <Grid 
+            container 
+            style={gridContainerStyle}
+        >
+            <Grid item>
+                <Stack alignItems='center'>
+                    <CardMedia
+                        component='img'
+                        src='media/RMELogo300-2-no-background.png' 
+                        alt='drum-logo' 
+                        style={imageStyle}
+                    />
+                    <CardMedia
+                        component='img'
+                        src='media/RMELogo300-2-titleonly-2.png'
+                        alt='rhythm-messengers'
+                        style={titleStyle}
+                    />
+                </Stack>
+            </Grid>
         </Grid>
     );
 }
