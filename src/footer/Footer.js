276e8a667902c0b0d2e8a9e693c0e94e1feb0708
Will Cooley <william.cooley@gmail.coom>
2024-02-05 20:21:40 -0500

    updated footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -14,8 +14,8 @@ function Footer(props) {
                 <Card
                   name="THOMAS BENKO"
                   title="Band Leader"
-                  email=""
-                  phoneNumber=" 1-630-415-9521" />
+                  email="tabdrums@gmail.com"
+                  phoneNumber="" />
               </Grid>
               <Grid item xs={12} sm={6} md={3} textAlign='center'>
                 <Card
@@ -23,13 +23,6 @@ function Footer(props) {
                   title="Agent - Dayna Malow"
                   email="info@liveartinternational.com"
                   phoneNumber="1-800-985-9330" />
-              </Grid>
-              <Grid item xs={12} sm={6} md={3} textAlign='center'>
-                <Card
-                  name="SF Agent"
-                  title="Pinnacle - Nathan Kelly"
-                  email=""
-                  phoneNumber="1-800-769-3322" />
               </Grid>
             </Grid>
             <Typography style={{fontSize: '12px', textAlign: 'center', marginBottom:'0', marginTop: '20px'}}>© 2024 Rhythm Messengers.</Typography>
