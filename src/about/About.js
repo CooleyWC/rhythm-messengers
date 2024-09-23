465cd80c4990aa7f9304515f870082ca394545a9
Will Cooley <william.cooley@gmail.com>
2024-09-22 21:17:13 -0400

    energy section complete

--- src/about/About.js
+++ src/about/About.js
@@ -43,11 +43,11 @@ const About=() =>{
       <Box>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={12} md={8} lg={8}>
-            <Typography variant="h2" align="center" gutterBottom sx={{ fontSize: '48px', color: '#007FFF', paddingTop: '70px', paddingBottom: '30px' }}>
+            <Typography variant="h2" align="center" gutterBottom sx={{ fontSize: {xs: '3rem', xl: '4rem'}, color: '#007FFF', paddingTop: '70px', paddingBottom: '30px' }}>
               Energy
             </Typography>
-            <BatteryChargingFullIcon style={{color: '#007FFF', fontSize:'60px', margin: '0 auto', display: 'block', paddingBottom: '50px'}}/>
-            <Typography variant='body1' paragraph sx={{color: 'white', fontSize: {xs:'1.2rem', sm: '1.3rem', md:'1.4rem', lg: '1.5rem', xl: '1.7rem'}, fontWeight: 'light', lineHeight: {xs: '1.4', sm: '1.3', md: '1.4'}, p: {xs: '0 2.7rem', sm: '1.5rem 5rem', md: '2rem 5.2rem 4rem 5.2rem', lg: '2.5rem 5.5rem 3rem 5.5rem', xl: '2.7rem 9rem'}, textAlign: 'center'}}>
+            <BatteryChargingFullIcon sx={{color: '#007FFF', fontSize: {xs: '4rem', sm: '4.5rem', md: '5rem', xl: '5.5rem'}, margin: '0 auto', display: 'block', paddingBottom: '50px'}}/>
+            <Typography variant='body1' paragraph sx={{color: 'white', fontSize: {xs:'1.2rem', sm: '1.3rem', lg: '1.4rem', xl: '1.6rem'}, fontWeight: 'light', lineHeight: {xs: '1.4', sm: '1.3', md: '1.4'}, p: {xs: '0 2.7rem 2.5rem 2.7rem', sm: '1.5rem 5rem', md: '2rem 5.2rem 4rem 5.2rem', lg: '2.5rem 7rem 5rem 7rem', xl: '2.7rem 12rem 5rem 12rem'}, textAlign: 'center'}}>
               Ignite your event with our choreographed, high energy drumming that will get your guests jumping out of their chairs!
             </Typography>
            
