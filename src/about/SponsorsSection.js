bf1fdc626ed6336cacd94a171f67811f23947657
ruth <ruro122020@gmail.com>
2024-09-30 18:21:32 -0400

    refactor code in sponsers section

--- src/about/SponsorsSection.js
+++ src/about/SponsorsSection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Grid, CardContent, Container, Divider } from "@mui/material";
+import sponsorsData from "./sponsersData";
 
 const SponsorsSection = () => {
   return (
@@ -14,258 +15,24 @@ const SponsorsSection = () => {
           paddingBottom: "20px",
         }}
       >
-        <Grid item>
-          <CardContent
-            component="img"
-            src="/media/client-logos/elmilagro.png"
-            alt="milagro"
-            loading="lazy"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "75px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            src="/media/client-logos/jra.png"
-            alt="jra"
-            loading="lazy"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "75px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/century21.png"
-            alt="c21"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "75px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/decision1.png"
-            alt="decision1"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/2friends.png"
-            alt="2friends"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/lolla.png"
-            alt="lolla"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/att.png"
-            alt="att"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/wintrust.png"
-            alt="wintrust"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/hand2mind.png"
-            alt="hand2mind"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/joeslive.png"
-            alt="joeslive"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/flyover.png"
-            alt="flyover"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/cabinet.png"
-            alt="cabinet"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/mri.png"
-            alt="mri"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/lifetime.png"
-            alt="lifetime"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/museum.png"
-            alt="museum"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/nurse.png"
-            alt="nurse"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/marathon.png"
-            alt="marathon"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
-        <Grid item>
-          <CardContent
-            component="img"
-            loading="lazy"
-            src="/media/client-logos/wbc.png"
-            alt="wbc"
-            style={{
-              padding: "0",
-              margin: "0",
-              maxHeight: "60px",
-              maxWidth: "100%",
-            }}
-          ></CardContent>
-        </Grid>
+        {sponsorsData.map(({ src, alt }) => {
+          return (
+            <Grid item>
+              <CardContent
+                component="img"
+                src={src}
+                alt={alt}
+                loading="lazy"
+                style={{
+                  padding: "0",
+                  margin: "0",
+                  maxHeight: "60px",
+                  maxWidth: "100%",
+                }}
+              ></CardContent>
+            </Grid>
+          );
+        })}
       </Grid>
       <Divider sx={{ height: 1, backgroundColor: "#CCCCCC" }} />
     </Container>
---
+++ src/about/sponsersData.js
@@ -0,0 +1,76 @@
+const sponsorsData = [
+  {
+    src: "/media/client-logos/elmilagro.png",
+    alt: "el milagro tortillas",
+  },
+  {
+    src: "/media/client-logos/jra.png",
+    alt: "jra just rite acoustics, inc.",
+  },
+  {
+    src: "/media/client-logos/century21.png",
+    alt: "century21",
+  },
+  {
+    src: "/media/client-logos/decision1.png",
+    alt: "decision One",
+  },
+  {
+    src: "/media/client-logos/2friends.png",
+    alt: "two friends",
+  },
+  {
+    src: "/media/client-logos/lolla.png",
+    alt: "lollapaloza",
+  },
+  {
+    src: "/media/client-logos/att.png",
+    alt: "AT&T",
+  },
+  {
+    src: "/media/client-logos/wintrust.png",
+    alt: "wintrust chicago's bank",
+  },
+  {
+    src: "/media/client-logos/hand2mind.png",
+    alt: "hand 2 mind",
+  },
+  {
+    src: "/media/client-logos/joeslive.png",
+    alt: "joe's live",
+  },
+  {
+    src: "/media/client-logos/flyover.png",
+    alt: "flyover",
+  },
+  {
+    src: "/media/client-logos/cabinet.png",
+    alt: "cabinet of curiosity",
+  },
+  {
+    src: "/media/client-logos/mri.png",
+    alt: "mri ascend north america 2023",
+  },
+  {
+    src: "/media/client-logos/lifetime.png",
+    alt: "lifetime fitness",
+  },
+  {
+    src: "/media/client-logos/museum.png",
+    alt: "museum of science + industry chicago",
+  },
+  {
+    src: "/media/client-logos/nurse.png",
+    alt: "american association of critical-care nurses",
+  },
+  {
+    src: "/media/client-logos/marathon.png",
+    alt: "life time chicago half marathon + 5k presented by HOKA",
+  },
+  {
+    src: "/media/client-logos/wbc.png",
+    alt: "world business chicago",
+  },
+];
+
+export default sponsorsData;
