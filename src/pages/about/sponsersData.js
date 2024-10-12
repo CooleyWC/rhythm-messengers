73cee8f08fd8339bd80a84428b86875fb24bb603
ruth <ruro122020@gmail.com>
2024-10-12 14:37:42 -0400

    convert images from png to webp

--- src/pages/about/sponsersData.js
+++ src/pages/about/sponsersData.js
@@ -1,74 +1,74 @@
 const sponsorsData = [
   {
-    src: "/media/client-logos/elmilagro.png",
+    src: "/media/client-logos/elmilagro.webp",
     alt: "el milagro tortillas",
   },
   {
-    src: "/media/client-logos/jra.png",
+    src: "/media/client-logos/jra.webp",
     alt: "jra just rite acoustics, inc.",
   },
   {
-    src: "/media/client-logos/century21.png",
+    src: "/media/client-logos/century21.webp",
     alt: "century21",
   },
   {
-    src: "/media/client-logos/decision1.png",
+    src: "/media/client-logos/decision1.webp",
     alt: "decision One",
   },
   {
-    src: "/media/client-logos/2friends.png",
+    src: "/media/client-logos/2friends.webp",
     alt: "two friends",
   },
   {
-    src: "/media/client-logos/lolla.png",
+    src: "/media/client-logos/lolla.webp",
     alt: "lollapaloza",
   },
   {
-    src: "/media/client-logos/att.png",
+    src: "/media/client-logos/att.webp",
     alt: "AT&T",
   },
   {
-    src: "/media/client-logos/wintrust.png",
+    src: "/media/client-logos/wintrust.webp",
     alt: "wintrust chicago's bank",
   },
   {
-    src: "/media/client-logos/hand2mind.png",
+    src: "/media/client-logos/hand2mind.webp",
     alt: "hand 2 mind",
   },
   {
-    src: "/media/client-logos/joeslive.png",
+    src: "/media/client-logos/joeslive.webp",
     alt: "joe's live",
   },
   {
-    src: "/media/client-logos/flyover.png",
+    src: "/media/client-logos/flyover.webp",
     alt: "flyover",
   },
   {
-    src: "/media/client-logos/cabinet.png",
+    src: "/media/client-logos/cabinet.webp",
     alt: "cabinet of curiosity",
   },
   {
-    src: "/media/client-logos/mri.png",
+    src: "/media/client-logos/mri.webp",
     alt: "mri ascend north america 2023",
   },
   {
-    src: "/media/client-logos/lifetime.png",
+    src: "/media/client-logos/lifetime.webp",
     alt: "lifetime fitness",
   },
   {
-    src: "/media/client-logos/museum.png",
+    src: "/media/client-logos/museum.webp",
     alt: "museum of science + industry chicago",
   },
   {
-    src: "/media/client-logos/nurse.png",
+    src: "/media/client-logos/nurse.webp",
     alt: "american association of critical-care nurses",
   },
   {
-    src: "/media/client-logos/marathon.png",
+    src: "/media/client-logos/marathon.webp",
     alt: "life time chicago half marathon + 5k presented by HOKA",
   },
   {
-    src: "/media/client-logos/wbc.png",
+    src: "/media/client-logos/wbc.webp",
     alt: "world business chicago",
   },
 ];
