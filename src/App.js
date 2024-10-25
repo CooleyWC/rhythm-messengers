5a5da59d8292d18cf3e822616188c402d54f64ba
ruth <ruro122020@gmail.com>
2024-10-25 13:16:53 -0400

    fixed bug, page scrolls back to contact after refresh

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import Video from "./Video";
 import About from "./pages/about/About";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
 
 function App() {
-  const aboutRef = React.useRef(null);
-  const contactRef = React.useRef(null);
+  const aboutRef = useRef(null);
+  const contactRef = useRef(null);
   const location = useLocation();
+  const [isRendered, setIsRendered] = useState(false);
+
+  useEffect(() => {
+    setIsRendered(true);
+  }, []);
 
   useEffect(() => {
     if (location.hash === "#Contact" && contactRef.current) {
@@ -26,7 +31,7 @@ function App() {
         inline: "start",
       });
     }
-  }, [location.hash]);
+  }, [location.hash, isRendered]);
 
   return (
     <div>
