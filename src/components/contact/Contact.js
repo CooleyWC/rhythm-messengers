cb7ed7196bf672ed31ec9c663319a5ee0d008665
ruth <ruro122020@gmail.com>
2024-11-02 02:32:57 -0400

    formik and yup installed and implemented for contact form validation

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,62 +3,60 @@ import Container from "@mui/material/Container";
 import emailjs from "@emailjs/browser";
 import CustomAlert from "./Alert";
 import Form from "./Form";
+import * as Yup from "yup";
+import { useFormik } from "formik";
 
 const Contact = () => {
-  const [formValues, setFormValues] = useState({
-    email: "",
-    message: "",
-  });
   const [isAlert, setIsAlert] = useState(null);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email("Must be a valid email")
+      .required("Please add email"),
+    message: Yup.string().required("Please add a message").max(300),
+  });
 
-    const serviceId = "service_g3c0vfn";
-    const templateId = "template_raegahg";
-    const publicKey = "airrzkTHnHtHmPrKl";
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      message: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values, { resetForm }) => {
+      const serviceId = "service_g3c0vfn";
+      const templateId = "template_raegahg";
+      const publicKey = "airrzkTHnHtHmPrKl";
 
-    const templateParams = {
-      from_email: formValues.email,
-      to_name: "Rhythm Messengers",
-      message: formValues.message,
-    };
+      const templateParams = {
+        from_email: values.email,
+        to_name: "Rhythm Messengers",
+        message: values.message,
+      };
 
-    emailjs
-      .send(serviceId, templateId, templateParams, {
-        publicKey: publicKey,
-      })
-      .then(
-        (res) => {
-          console.log("success", res);
-          setFormValues({
-            email: "",
-            message: "",
-          });
-          setIsAlert(true);
-        },
-        (error) => {
-          console.error("error", error);
-          setIsAlert(false);
-        }
-      );
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues((prevState) => ({ ...prevState, [name]: value }));
-  };
+      emailjs
+        .send(serviceId, templateId, templateParams, {
+          publicKey: publicKey,
+        })
+        .then(
+          (res) => {
+            console.log("success", res);
+            resetForm();
+            setIsAlert(true);
+          },
+          (error) => {
+            console.error("error", error);
+            setIsAlert(false);
+          }
+        );
+    },
+  });
 
   return (
     <Container
-      sx={{ color: "white", paddingBottom: "46px", paddingTop: "46px"}}
+      sx={{ color: "white", paddingBottom: "46px", paddingTop: "46px" }}
     >
       <CustomAlert isAlert={isAlert} />
-      <Form
-        handleSubmit={handleSubmit}
-        handleChange={handleChange}
-        formValues={formValues}
-      />
+      <Form formik={formik} />
     </Container>
   );
 };
