62bcd6bb8ea2cc6597e00880dbed069f4b8d85e4
ruth <ruro122020@gmail.com>
2024-11-02 23:47:45 -0400

    removed gradiant

--- src/pages/about/SponsorsSection.js
+++ src/pages/about/SponsorsSection.js
@@ -27,11 +27,7 @@ const SponsorsSection = () => {
   return (
     <>
       <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          paddingTop: "46px",
-        }}
+        sx={{ display: "flex", justifyContent: "center", paddingTop: "46px" }}
       >
         <Typography
           variant="h2"
@@ -55,24 +51,24 @@ const SponsorsSection = () => {
           "&:hover > .innerContainer": {
             animationPlayState: "paused",
           },
-          "&:before, &:after": {
-            position: "absolute",
-            width: "150px",
-            height: "100%",
-            top: 0,
-            zIndex: "2",
-            content: '""',
-          },
-          "&:before": {
-            left: 0,
-            background:
-              "linear-gradient(to left, rgba(255,255,255, 0), #141A1F)",
-          },
-          "&:after": {
-            right: 0,
-            background:
-              "linear-gradient(to right, rgba(255,255,255, 0), #141A1F)",
-          },
+          // "&:before, &:after": {
+          //   position: "absolute",
+          //   width: "150px",
+          //   height: "100%",
+          //   top: 0,
+          //   zIndex: "2",
+          //   content: '""',
+          // },
+          // "&:before": {
+          //   left: 0,
+          //   background:
+          //     "linear-gradient(to left, rgba(255,255,255, 0), #141A1F)",
+          // },
+          // "&:after": {
+          //   right: 0,
+          //   background:
+          //     "linear-gradient(to right, rgba(255,255,255, 0), #141A1F)",
+          // },
         }}
       >
         <Box
@@ -82,7 +78,7 @@ const SponsorsSection = () => {
             animation: `65s infinite linear ${slide}`,
           }}
         >
-          {[...sponsorsData, ...sponsorsData].map(({ src, alt }, index) => (
+          {sponsorsData.map(({ src, alt }, index) => (
             <Box key={index} sx={cardStyle}>
               <Box component="img" sx={imgStyle} src={src} alt={alt} />
             </Box>
