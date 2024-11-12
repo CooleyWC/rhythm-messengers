8bb11241a079b80dc4ebf6bcec119642960bdb1b
Will Cooley <william.cooley@gmail.com>
2024-11-11 21:12:55 -0500

    updated contact.js and workflows.

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -37,7 +37,7 @@ const Contact = () => {
           process.env.REACT_APP_TEMPLATEID,
           templateParams,
           {
-            publicKey: "airrzkTHnHtHmPrKl",
+            publicKey: process.env.REACT_APP_PUBLIC_KEY,
           }
         )
         .then((res) => {
