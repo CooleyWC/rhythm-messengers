4f88bd36669eaa0f5ab3abbac598485739a9362a
Will Cooley <william.cooley@gmail.com>
2024-11-11 20:45:05 -0500

    contact form test 11-11-24

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -37,7 +37,7 @@ const Contact = () => {
           process.env.REACT_APP_TEMPLATEID,
           templateParams,
           {
-            publicKey: process.env.REACT_APP_PUBLIC_KEY,
+            publicKey: "airrzkTHnHtHmPrKl",
           }
         )
         .then((res) => {
