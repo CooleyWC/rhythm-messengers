f86689d011094c9f276621b946dc2d5c2f7989cf
Will Cooley <william.cooley@gmail.com>
2024-11-26 13:49:22 -0500

    changed display back to grid

--- src/components/footer/Card.js
+++ src/components/footer/Card.js
@@ -4,10 +4,10 @@ import React from 'react'
 const Card = ({name, title, email, phoneNumber}) => {
   return (
     <div>
-      <Typography sx={{fontSize: '1rem'}}>{name}</Typography>
-      <Typography sx={{fontSize: '0.9rem', fontWeight: 350}}>{title}</Typography>
-      <Typography sx={{fontSize: '0.9rem', fontWeight: 350}}>{email}</Typography>
-      {phoneNumber.length > 0 && <Typography sx={{fontSize: '0.9rem', fontWeight: 350}}>{phoneNumber}</Typography>}
+      <Typography sx={{fontSize: {xs:'0.88rem', sm:'1rem'}, fontWeight: 700}}>{name}</Typography>
+      <Typography sx={{fontSize: {xs:'0.75rem',sm:'0.95rem'}, fontWeight: 400}}>{title}</Typography>
+      <Typography sx={{fontSize: {xs:'0.75rem', sm:'0.9rem'}, fontWeight: 400}}>{email}</Typography>
+      {phoneNumber.length > 0 && <Typography sx={{fontSize: {xs:'0.75rem',sm:'0.9rem'}, fontWeight: 400}}>{phoneNumber}</Typography>}
     </div>
   )
 }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,58 +1,36 @@
 import React from 'react';
 import Box from '@mui/material/Box'
-// import Grid from '@mui/material/Grid'
-import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid'
 import Card from './Card'
 import { Typography } from '@mui/material';
 
 function Footer() {
     return (
-          <Box sx={{backgroundColor:'#f5f5f5', paddingTop: '20px' }}>
-            <Box sx={{
-              display: 'flex', 
-              flexDirection: 'row', 
-              justifyContent: 'center', gap: {xs:'2rem', sm:'4rem'},
-              // alignItems: {xs:'center', sm:'flex-start'},
-              textAlign: 'left',
-              fontSize: {xs:'0.5rem', sm:'1rem'},
-          
-            
-              }}>
+          <Box sx={{backgroundColor:'#f5f5f5'}}>
+            <Grid container spacing={2} 
+            sx={{justifyContent: 'center'
+            }}>
+              <Grid item xs={12} sm={6} md={3} textAlign='left' sx={{maxWidth:{xs:'200px', sm:'none'}}}>
                 <Card
                   name="THOMAS BENKO"
                   title="Band Leader"
                   email="tabdrums@gmail.com"
-                  phoneNumber=""
+                  phoneNumber="" 
                   />
-                <Divider orientation='vertical' sx={{border:'0.8px solid', height: '75px'}}/>
+              </Grid>
+              <Grid item xs={12} sm={6} md={3} textAlign='left' sx={{maxWidth:{xs:'200px', sm:'none'}}}>
                 <Card
                   name="LIVE ART INTERNATIONAL"
                   title="Agent - Dayna Malow"
                   email="dayna@liveartinternational.com"
-                  phoneNumber="1-800-985-9330" />
-            </Box>
-            <Typography style={{fontSize: '12px', textAlign: 'center', marginBottom:'0', marginTop: '20px'}}>© 2024 Rhythm Messengers. | Designed by BlueWave Codeworks.</Typography>
+                  phoneNumber="1-800-985-9330" 
+                  />
+              </Grid>
+            </Grid>
+            <Typography style={{fontSize: '0.75rem', textAlign: 'center', marginBottom:'0', marginTop: '30px'}}>© 2024 Rhythm Messengers. Designed by BlueWave Codeworks.</Typography>
           </Box>
     );
 }
 
 export default Footer;
 
-
-// <Grid container spacing={2} sx={{justifyContent: 'center'}}>
-//               <Grid item xs={12} sm={6} md={3} textAlign='left' sx={{maxWidth:{xs:'200px', sm:'none'}}}>
-//                 <Card
-//                   name="THOMAS BENKO"
-//                   title="Band Leader"
-//                   email="tabdrums@gmail.com"
-//                   phoneNumber="" />
-//               </Grid>
-//               <Grid item xs={12} sm={6} md={3} textAlign='left' sx={{maxWidth:{xs:'200px', sm:'none'}}}>
-//                 <Card
-//                   name="LIVE ART INTERNATIONAL"
-//                   title="Agent - Dayna Malow"
-//                   email="dayna@liveartinternational.com"
-//                   phoneNumber="1-800-985-9330" />
-//               </Grid>
-//             </Grid>
-//             <Typography style={{fontSize: '12px', textAlign: 'center', marginBottom:'0', marginTop: '20px'}}>© 2024 Rhythm Messengers. | Designed by BlueWave Codeworks.</Typography>
