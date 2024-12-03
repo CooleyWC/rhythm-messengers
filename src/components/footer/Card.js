b761535b6a79907455dd908667699d74ad280d05
Will Cooley <william.cooley@gmail.com>
2024-12-02 21:16:10 -0500

    updated text color and card spacing

--- src/components/footer/Card.js
+++ src/components/footer/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Card = ({name, title, email, phoneNumber}) => {
   return (
-    <Box sx={{width: '216px', px:4, py:{xs:3, sm:4},}}>
+    <Box sx={{width: '216px', px:{xs:4, lg:5}, py:{xs:3, sm:4},}}>
       <Typography sx={{fontSize: {xs:'0.95rem', sm:'1rem'}, fontWeight: 700}}>{name}</Typography>
       <Typography sx={{fontSize: {xs:'0.9rem',sm:'0.95rem'}, fontWeight: 400}}>{title}</Typography>
       <Typography sx={{fontSize: {xs:'0.9rem', sm:'0.95rem'}, fontWeight: 400}}>{email}</Typography>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,34 +5,35 @@ import { Typography } from '@mui/material';
 
 function Footer() {
     return (
-        <>
-      <Box sx={{display:'flex', 
-        flexDirection:{xs:'column', sm:'row'}, 
-        justifyContent: 'center',
-        alignItems: {xs:'center', sm: 'start'},
-        backgroundColor:'#f5f5f5',
-      }}>
-        <Card
+      <>
+        <Box sx={{display:'flex', 
+          flexDirection:{xs:'column', sm:'row'}, 
+          justifyContent: 'center',
+          alignItems: {xs:'center', sm: 'start'},
+          backgroundColor:'#f5f5f5',
+          color: '#4a4a4a'
+        }}>
+          <Card
             name="THOMAS BENKO"
             title="Band Leader"
             email="tabdrums@gmail.com"
             phoneNumber=""
-        >
-        </Card>
-        <Card
+          >
+          </Card>
+          <Card
             name="LIVE ART INTERNATIONAL"
             title="Agent - Dayna Malow"
             email="dayna@liveartinternational.com"
             phoneNumber="1-800-985-9330"
-        >
-        </Card>   
-      </Box>
+          >
+          </Card>   
+        </Box>
         <Box sx={{display: 'flex', justifyContent:'center', alignItems: 'center', backgroundColor: '#e0e0e0'}}>
             <Typography sx={{fontSize: '0.75rem', py:1.5, px:{xs:2}, textAlign: 'center'}}>
                 © 2024 Rhythm Messengers. Designed by BlueWave Codeworks.
             </Typography>
         </Box>
-    </>   
+      </>   
     );
 }
 
