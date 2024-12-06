2dd4a9f52927bdfeda9e2e97c469bb5e9228761f
Will Cooley <william.cooley@gmail.com>
2024-12-05 23:02:23 -0500

    image update in progress

--- src/Title.js
+++ src/Title.js
@@ -1,54 +1,115 @@
 import React from "react";
-import { Grid, CardMedia } from "@mui/material";
-import Stack from "@mui/material/Stack";
+import { CardMedia, Box } from "@mui/material";
+
 
 function Title() {
-  const gridContainerStyle = {
-    backgroundColor: "black",
-    margin: 0,
-    padding: 0,
-  };
 
-  const imageStyle = {
-    maxWidth: "55%",
-    maxHeight: "55%",
-    width: "auto",
-    height: "auto",
-    marginTop: "25px",
-    objectFit: "cover",
-  };
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    backgroundColor: 'black',
+    gap: '1rem'
+  }
+  
+  const drumStyle = {
+    marginTop: {xs:0, sm:'4rem', md:'5rem', lg:'6rem', xl: '8rem'},
+    maxWidth:{xs:'100%', md:'90%', lg: '50%', xl: '40%'},
+    objectFit: 'contain',
+    
+  }
 
   const titleStyle = {
-    maxWidth: "75%",
-    marginTop: "-55px",
-    width: "auto",
-    height: "auto",
-  };
+    maxWidth:{xs:'90%', md:'80%', lg: '50%', xl: '40%'},
+    objectFit: 'contain',
+  }
+
+  
 
   return (
-    <Grid container style={gridContainerStyle}>
-      <Grid item>
-        <Stack alignItems="center">
+    <Box sx={containerStyle}>
           <CardMedia
+            fetchpriority='high'
             component="img"
-            src="media/RMELogo300-2-no-background.webp"
+            src="media/RMELogo300-2-no-background-opt-cr.webp"
             alt="drum-logo"
-            style={imageStyle}
-            width={300}
-            height={300}
+            sx={drumStyle}
+            width={1341}
+            height={1145}
           />
           <CardMedia
             component="img"
-            width={300}
-            height={300}
-            src="media/RMELogo300-2-titleonly-2.webp"
+            width={1200}
+            height={337}
+            src="media/RMELogo300-2-titleonly-2-opt.webp"
             alt="rhythm-messengers"
-            style={titleStyle}
+            sx={titleStyle}
           />
-        </Stack>
-      </Grid>
-    </Grid>
+    </Box>
   );
 }
 
 export default Title;
+
+
+// const gridContainerStyle = {
+//   // backgroundColor: "black",
+//   // display: 'flex',
+//   // flexDirection: 'column',
+//   margin: 0,
+//   padding: 0,
+//   justifyContent: 'center',
+//   alignItems: 'center',
+//   height: '100vh'
+// };
+
+// const imageStyle = {
+//   // maxWidth: "55%",
+//   maxHeight: "55%",
+//   // width: "auto",
+//   // height: "auto",
+//   // marginTop: "5rem",
+//   objectFit: "cover",
+// };
+
+// const titleStyle = {
+//   // maxWidth: "75%",
+//   // marginTop: "-55px",
+//   // width: "auto",
+//   // height: "auto",
+// };
+
+// return (
+//   <Grid container direction='column' sx={gridContainerStyle}>
+//     <Grid item sx={{marginTop: '4rem'}}>
+//         <CardMedia
+//           fetchpriority='high'
+//           component="img"
+//           src="media/RMELogo300-2-no-background-opt-cr.webp"
+//           // src="media/RMELogo300-2-no-background.webp"
+//           alt="drum-logo"
+//           style={imageStyle}
+//           // width={300}
+//           // height={300}
+          
+//         />
+//     </Grid>
+//     <Grid item>
+//       <div>
+//         <h1>hello</h1>
+//       </div>
+//         {/* <CardMedia
+//           component="img"
+//           // width={300}
+//           // height={300}
+//           src="media/RMELogo300-2-titleonly-2-optimized.webp"
+//           // src="media/RMELogo300-2-titleonly-2.webp"
+//           alt="rhythm-messengers"
+//           style={titleStyle}
+//         /> */}
+//     </Grid>
+//   </Grid>
+// );
+// }
