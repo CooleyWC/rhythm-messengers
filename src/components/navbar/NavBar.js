d3bcf9cafe9f05ee8ab1fad149446d18674d231f
Will Cooley <william.cooley@gmail.com>
2024-12-06 23:42:32 -0500

    nav-update in progress

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -25,7 +25,7 @@ function NavBar() {
   };
 
   return (
-    <AppBar style={{ position: "fixed" }} sx={{ backgroundColor: "#f77a00" }}>
+    <AppBar style={{ position: "fixed" }} sx={{ backgroundColor: "#f77a00"}}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           {/* This Typography element renders on desktop screen size */}
@@ -39,8 +39,9 @@ function NavBar() {
               display: { xs: "none", md: "flex" },
               fontFamily: "monospace",
               fontWeight: 700,
+              fontSize: '1.3rem',
               letterSpacing: ".3rem",
-              color: "inherit",
+              color: "#02010a",
               textDecoration: "none",
             }}
           >
@@ -55,7 +56,7 @@ function NavBar() {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               onClick={handleOpenNavMenu}
-              color="inherit"
+              color="#02010a"
             >
               <MenuIcon />
             </IconButton>
@@ -74,7 +75,7 @@ function NavBar() {
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: "block", md: "none" },
+                display: { xs: "block", md: "none" }
               }}
             >
               {pages.map(({ page, route }) => (
@@ -95,9 +96,10 @@ function NavBar() {
               display: { xs: "flex", md: "none" },
               flexGrow: 1,
               fontFamily: "monospace",
+              fontSize: {xs:'1rem', sm: '1.2rem', md:'1.3rem'},
               fontWeight: 700,
               letterSpacing: ".3rem",
-              color: "inherit",
+              color: "#02010a",
               textDecoration: "none",
               overflow: "visible",
             }}
@@ -119,14 +121,17 @@ function NavBar() {
                 component={Link}
                 to={route}
                 sx={{
+                  fontWeight:500,
+                  fontSize: '1rem',
+                  letterSpacing: 2,
                   my: 2,
-                  color: "white",
+                  color: "#02010a",
                   "&:hover": {
                     backgroundColor: "rgba(25, 118, 210, 0.13) !important",
                   },
                 }}
               >
-                {page === "Instagram" ? <InstagramIcon /> : page}
+                {page === "Instagram" ? <InstagramIcon sx={{fontSize:'1.7rem'}}/> : page}
               </Button>
             ))}
           </Box>
